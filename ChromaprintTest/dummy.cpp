fcc5ca6aa8079915efad67e2d1e1f29660e4c612
Teo Sartori <m@sartori.dk>
2016-01-10 17:56:44 +0100

    Explain dummy.cpp

--- ChromaprintTest/dummy.cpp
+++ ChromaprintTest/dummy.cpp
@@ -2,8 +2,13 @@
 //  dummy.cpp
 //  ChromaprintTest
 //
+//  Dummy file to fool Xcode into using the clang++ compiler and to 
+//  add the -stdlib=libc++ flag needed to properly link the C++ Chromaprint library. 
+//
 //  Created by teo on 08/01/16.
 //  Copyright © 2016 Terminal Glow. All rights reserved.
 //
+//  Licensed under MIT See LICENCE file in the root of this project for details.
+
 
 #include <stdio.h>
